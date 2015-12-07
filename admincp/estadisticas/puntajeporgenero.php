cbec1954296ff186ce9b52700f42d6b3fae8522d
juancho <thejuasz@gmail.com>
2015-12-06 23:16:43 -0300

    Titulo cambiado en puntaje genero

--- admincp/estadisticas/puntajeporgenero.php
+++ admincp/estadisticas/puntajeporgenero.php
@@ -21,7 +21,7 @@ require_once("../../includes/jgraph/jpgraph_line.php");
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Agregar Juego</title>
+<title>Mejores puntajes por genero!</title>
 <meta name="keywords" content="" />
 <meta name="description" content="" />
 <link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,700,900" rel="stylesheet" />
